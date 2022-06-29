4431f6eb58b388ede1ace12d75da6f65457947f6
Andrei800 <vedernikov485@gmail.com>
2022-06-29 21:09:34 +0200

    1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -73,7 +73,7 @@ DATABASES = {
         'PASSWORD': os.getenv(
             'POSTGRES_PASSWORD', default='postgres'),
         'HOST': os.getenv(
-            'DB_HOST', default='localhost'),
+            'DB_HOST', default='127.0.0.1'),
         'PORT': os.getenv(
             'DB_PORT', default='5432'),
     }
