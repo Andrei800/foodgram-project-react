d26827b8d29816ce0dca81bdb488f061c0f9d3c5
Andrei800 <vedernikov485@gmail.com>
2022-06-29 10:39:34 +0200

    1

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -1,11 +1,5 @@
-
-"""Creating the paginator."""
-
 from rest_framework.pagination import PageNumberPagination
 
-
 class CustomPagination(PageNumberPagination):
-    """Creating the paginator inherited from PageNumberPagination."""
-
     page_size = 6
     page_size_query_param = 'limit'
