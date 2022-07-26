acc59f4d492604fb5fc665fae4de0acb043d08e9
Andrei800 <vedernikov485@gmail.com>
2022-07-26 18:03:57 +0200

    Admin update

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import (Favorite, Ingredient, IngredientInRecipe, Recipe,
+from recipes.models import (Favorite, Ingredient, IngredientInRecipe, Recipe,
                      ShoppingCart, Tag, TagRecipe)
 
 
